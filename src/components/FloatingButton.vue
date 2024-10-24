f0baef6c2a1ae7026f5554c0426da996e01a9c51
Pandu Sabdanabi <sabdanabi>
2024-10-24 16:11:24 +0700

    fix-popup

---
+++ src/components/FloatingButton.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+import {Icon} from "@iconify/vue";
+</script>
+
+<template>
+  <button class="bg-green-500 w-16 h-16 text-lg flex items-center justify-center
+  rounded-t-full rounded-br-full ml-5 fixed bottom-16 right-10 z-[9999]">
+    <a href="https://api.whatsapp.com/send?phone=6282138395289"><Icon icon="ic:baseline-whatsapp" width="35" height="35" class="text-white"/></a>
+  </button>
+</template>
+<style scoped>
+
+</style>
---
+++ src/views/Intructios.vue
@@ -0,0 +1,89 @@
+<script setup>
+
+</script>
+
+<template>
+  <section id="intructio-section" class="container px-32 font-poppins py-24">
+    <div class="relative flex justify-center items-center">
+      <div class="text-center">
+        <p class="text-4xl">Cara Kerja Alat Panic Button</p>
+        <article class=" mt-5 w-[530px] ml-3 text-mediumGrey">
+          Aplikasi Tombol Panik adalah sebuah inovasi
+          untuk meningkatkan respons terhadap situasi darurat dan kejahatan.</article>
+      </div>
+    </div>
+    <div class="mt-32 flex gap-52 relative">
+      <div>
+        <span class="bg-paradisePink px-6 py-4 rounded-lg text-white font-medium text-5xl">01</span>
+        <p class="mt-10 text-lg font-semibold">Download Aplikasi Panic Button </p>
+        <p class="text-sm w-80 font-medium text-mediumGrey">Download Aplikasi Panic Button di
+          PlayStore atau AppStore lalu install di smart phone anda.</p>
+      </div>
+
+      <span class="absolute left-48">
+        <img src="/assets_image/assets_image_intructios_section/arrow-down-1.png" alt="image-arrow" class="w-72">
+      </span>
+
+      <div class="mt-28">
+        <span class="bg-tuftsBlu px-6 py-4 rounded-lg text-white font-medium text-5xl">02</span>
+        <p class="mt-10 text-lg font-semibold">Permintaan Pasang Panic Button</p>
+        <p class="text-sm w-80 font-medium text-mediumGrey">
+          Pengguna dapat melakukan permintaan kepada
+          petugas untuk memasang Alat Panic Button dari aplikasi</p>
+      </div>
+
+      <span class="absolute right-72 top-10">
+        <img src="/assets_image/assets_image_intructios_section/arrow-up-1.png" alt="image-arrow" class="w-60">
+      </span>
+
+      <div>
+        <span class="bg-mediumSlateBlue px-6 py-4 rounded-lg text-white font-medium text-5xl">03</span>
+        <p class="mt-10 text-lg font-semibold">Petugas datang</p>
+        <p class="text-sm w-72 font-medium text-mediumGrey">
+          Petugas datang untuk melakukan pemasangan Alat Panic Button</p>
+      </div>
+    </div>
+    <div class="mt-28">
+      <div class="relative flex justify-center items-center">
+        <p class="text-4xl">Cara Kerja Alat Panic Button</p>
+      </div>
+      <div class="mt-28 flex gap-52 relative">
+        <div>
+          <span class="bg-mediumAquamarine px-6 py-4 rounded-lg text-white font-medium text-5xl">04</span>
+          <p class="mt-10 text-lg font-semibold">Tekan Tombol pada Alat Panic Button</p>
+          <p class="text-sm w-80 font-medium text-mediumGrey">
+            Pengguna dapat menekan tombol pada Alat Panic Button,
+            untuk mengirim sinyal kepada petugas.</p>
+        </div>
+
+        <span class="absolute left-56">
+        <img src="/assets_image/assets_image_intructios_section/arrow-down-2.png" alt="image-arrow" class="w-60">
+        </span>
+
+        <div class="mt-28">
+          <span class="bg-burntSienna px-6 py-4 rounded-lg text-white font-medium text-5xl">05</span>
+          <p class="mt-10 text-lg font-semibold">Petugas konfirmasi  Permintaan</p>
+          <p class="text-sm w-80 font-medium text-mediumGrey">
+            Petugas mengkonfirmasi permintaan,
+            lalu petugas datang kelokasi tiitik panic button.</p>
+        </div>
+
+        <span class="absolute right-72 top-10">
+        <img src="/assets_image/assets_image_intructios_section/arrow-up-2.png" alt="image-arrow" class="w-56">
+      </span>
+
+        <div>
+          <span class="bg-mediumOrchid px-6 py-4 rounded-lg text-white font-medium text-5xl">06</span>
+          <p class="mt-10 text-lg font-semibold">Review dari Aplikasi</p>
+          <p class="text-sm w-72 font-medium text-mediumGrey">
+            Setelah petugas menyelesaikan permintaan,
+            pengguna dapat memberi review dari Aplikasi Panic Button.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
