82780ef92429df90d5314ff2dfc1e852a29f7f0d
Pandu Sabdanabi <sabdanabi>
2024-10-30 14:18:04 +0700

    fix intruction

--- src/views/Intructios.vue
+++ src/views/Intructios.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-  <section id="intructio-section" class="container lg:px-32 md:px-16 px-12 xl:px-32 font-poppins py-24">
+  <section id="intructio-section" class="container lg:px-24 md:px-16 px-12 xl:px-32 font-poppins py-24">
 
     <div class="relative flex justify-center items-center md:mt-20">
       <div class="text-center">
@@ -17,40 +17,48 @@
 
       <div>
         <span class="bg-paradisePink style-number">01</span>
-        <p class="xl:mt-10 lg:mt-7 lg:text-base md:text-sm font-semibold md:w-64 md:mt-6 w-72 mt-2">
-          Download Aplikasi Panic Button
-        </p>
-        <p class="style-desc-intruction md:w-52 w-64">
-          Download Aplikasi Panic Button di PlayStore atau AppStore lalu install di smartphone Anda.
-        </p>
+        <div class="style-border-intruction">
+          <p class="xl:mt-10 lg:mt-7 lg:text-base md:text-sm font-semibold md:w-64 w-72 mt-2 md:mt-0">
+            Download Aplikasi Panic Button
+          </p>
+          <p class="style-desc-intruction md:w-52 w-64">
+            Download Aplikasi Panic Button di PlayStore atau AppStore lalu install di smartphone Anda.
+          </p>
+        </div>
       </div>
 
-      <span class="absolute xl:left-72 lg:left-48 md:left-24 md:-top-3 hidden md:block">
-        <img src="/assets_image/assets_image_intructios_section/arrow-down-1.png" alt="image-arrow" class="xl:w-72 lg:w-48 md:w-44">
+      <span class="absolute xl:left-72 lg:left-48 md:left-32 md:-top-3 hidden md:block">
+        <img src="/assets_image/assets_image_intructios_section/arrow-down-1.png" alt="image-arrow" class="xl:w-72 lg:w-44 md:w-44">
       </span>
 
       <div class="md:mt-28 my-5">
         <span class="bg-tuftsBlu style-number">02</span>
-        <p class="xl:mt-10 text-lg font-semibold md:text-sm md:w-64 md:mt-6 w-72 mt-2">
-          Permintaan Pasang Panic Button
-        </p>
-        <p class="style-desc-intruction md:w-60 w-64">
-          Pengguna dapat melakukan permintaan kepada petugas untuk memasang Alat Panic Button.
-        </p>
+        <div class="style-border-intruction">
+          <p class="xl:mt-10 text-lg font-semibold md:text-sm md:w-64 w-72 mt-2 md:mt-0">
+            Permintaan Pasang Panic Button
+          </p>
+          <p class="style-desc-intruction md:w-60 w-64">
+            Pengguna dapat melakukan permintaan kepada petugas untuk memasang Alat Panic Button.
+          </p>
+        </div>
       </div>
 
-      <span class="absolute xl:right-60 lg:right-36 top-10 md:right-36 hidden md:block">
-        <img src="/assets_image/assets_image_intructios_section/arrow-up-1.png" alt="image-arrow" class="xl:w-60 lg:w-40 md:w-40">
+      <span class="absolute xl:right-60 lg:right-56 top-14 md:right-36 hidden md:block">
+        <img src="/assets_image/assets_image_intructios_section/arrow-up-1.png" alt="image-arrow" class="xl:w-60 lg:w-32 md:w-32">
       </span>
 
-      <div>
-        <span class="bg-mediumSlateBlue style-number">03</span>
-        <p class="xl:mt-10 text-lg font-semibold md:text-sm md:w-64 md:mt-6 mt-2">
-          Petugas datang
-        </p>
-        <p class="style-desc-intruction md:w-32 w-64">
-          Petugas datang untuk melakukan pemasangan Alat Panic Button.
-        </p>
+      <div class="md:relative">
+        <div class="md:absolute md:-right-20 md:bottom-36">
+          <span class="bg-mediumSlateBlue style-number">03</span>
+          <div class="style-border-intruction">
+            <p class="xl:mt-10 text-lg font-semibold md:text-sm md:w-36 md:mt-0 mt-2">
+              Petugas datang
+            </p>
+            <p class="style-desc-intruction md:w-32 w-64">
+              Petugas datang untuk melakukan pemasangan Alat Panic Button.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
 
@@ -59,43 +67,51 @@
         <p class="lg:text-4xl md:text-2xl text-center font-medium">Cara Kerja Alat Panic Button</p>
       </div>
 
-      <div class="md:mt-28 xl:mt-28 lg:mt-28 mt-10 md:flex xl:gap-52 lg:gap-10 relative xl:px-40 lg:px-20">
+      <div class="md:mt-28 xl:mt-28 lg:mt-28 mt-10 md:gap-5 md:flex xl:gap-52 lg:gap-10 relative xl:px-40 lg:px-20">
         <div>
           <span class="bg-mediumAquamarine style-number">04</span>
-          <p class="mt-2 xl:mt-10 text-lg font-semibold md:text-sm md:w-64 md:mt-6 w-72">
-            Tekan Tombol pada Alat Panic Button
-          </p>
-          <p class="style-desc-intruction text-mediumGrey md:w-56 w-64">
-            Pengguna dapat menekan tombol pada Alat Panic Button untuk mengirim sinyal kepada petugas.
-          </p>
+          <div class="style-border-intruction">
+            <p class="mt-2 md:mt-0 xl:mt-10 text-lg font-semibold md:text-sm md:w-64 w-72">
+              Tekan Tombol pada Alat Panic Button
+            </p>
+            <p class="style-desc-intruction text-mediumGrey md:w-56 w-64">
+              Pengguna dapat menekan tombol pada Alat Panic Button untuk mengirim sinyal kepada petugas.
+            </p>
+          </div>
         </div>
 
-        <span class="absolute xl:left-80 lg:left-56 md:left-32 hidden md:block">
+        <span class="absolute xl:left-80 lg:left-60 md:left-44 hidden md:block">
           <img src="/assets_image/assets_image_intructios_section/arrow-down-2.png" alt="image-arrow" class="xl:w-60 lg:w-40 md:w-36">
         </span>
 
         <div class="md:mt-28 my-5">
           <span class="bg-burntSienna style-number">05</span>
-          <p class="mt-2 xl:mt-10 text-lg font-semibold md:text-sm md:w-64 md:mt-6 w-72">
-            Petugas konfirmasi Permintaan
-          </p>
-          <p class="style-desc-intruction md:w-[234px] w-64">
-            Petugas mengkonfirmasi permintaan, lalu petugas datang ke lokasi titik panic button.
-          </p>
+          <div class="style-border-intruction">
+            <p class="mt-2 xl:mt-10 text-lg font-semibold md:text-sm md:w-64 md:mt-0 w-72">
+              Petugas konfirmasi Permintaan
+            </p>
+            <p class="style-desc-intruction md:w-[234px] w-64">
+              Petugas mengkonfirmasi permintaan, lalu petugas datang ke lokasi titik panic button.
+            </p>
+          </div>
         </div>
 
-        <span class="absolute xl:right-60 lg:right-36 top-10 md:right-40 hidden md:block">
-          <img src="/assets_image/assets_image_intructios_section/arrow-up-2.png" alt="image-arrow" class="xl:w-52 lg:w-40 md:w-32">
+        <span class="absolute xl:right-60 lg:right-60 top-16 md:right-44 hidden md:block">
+          <img src="/assets_image/assets_image_intructios_section/arrow-up-2.png" alt="image-arrow" class="xl:w-52 lg:w-28 md:w-28">
         </span>
 
-        <div>
-          <span class="bg-mediumOrchid style-number">06</span>
-          <p class="mt-2 xl:mt-10 text-lg font-semibold md:text-sm md:w-64 md:mt-6 w-72">
-            Review dari Aplikasi
-          </p>
-          <p class="style-desc-intruction md:w-36 w-64">
-            Setelah petugas selesaikan permintaan, pengguna dapat memberi review dari Aplikasi.
-          </p>
+        <div class="md:relative">
+          <div class="md:absolute md:-right-20 md:bottom-36">
+            <span class="bg-mediumOrchid style-number">06</span>
+            <div class="style-border-intruction">
+              <p class="mt-2 xl:mt-10 text-lg font-semibold md:text-sm md:w-40 md:mt-0 w-72">
+                Review dari Aplikasi
+              </p>
+              <p class="style-desc-intruction md:w-36 w-64">
+                Setelah petugas selesaikan permintaan, pengguna dapat memberi review dari Aplikasi.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
