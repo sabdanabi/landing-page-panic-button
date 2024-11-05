dd3adbeec4ed7360c410490c580d13109c6cfbec
Pandu Sabdanabi <sabdanabi>
2024-11-05 16:04:35 +0700

    fix

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -18,9 +18,9 @@
 </script>
 
 <template>
-      <nav class="mx-auto flex justify-between items-center pt-4 font-poppins px-16 z-10 fixed bg-white drop-shadow-lg">
+      <nav class="mx-auto flex justify-between items-center w-full pt-4 font-poppins px-16 z-10 fixed bg-white drop-shadow-lg">
         <img src="/assets_image/logo-panic-button.png" alt="image-logo"
-             class="w-16 mb-2 md:mr-20 3xl:mr-[1067px] xl:mr-[560px] lg:mr-72 2xl:w-20">
+             class="w-16 mb-2 2xl:w-20">
         <div class="flex justify-between items-center 3xl:gap-36 lg:gap-1 md:gap-10 2xl:gap-20">
           <ul class="flex space-x-9 xl:mr-10 3xl:gap-10 2xl:text-base xl:text-sm 3xl:text-2xl
           md:text-xs lg:mr-10 text-mediumGrey lg:text-sm">
